af966bdb6c442f294ea0fbcecbf2afd59058dc28
張政傑 <sa841218as@gmail.com>
2019-04-30 23:49:50 +0800

    heroku/frontend/新增 UserConsolePanel (開發中)

---
+++ heroku/frontend/src/components/ConsoleTable/UserConsolePanel/index.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './UserConsolePanel.scss'
+
+class UserConsolePanel extends Component {
+  render() {
+    return (
+      <div id="user-console-panel">
+        User
+      </div>
+    )
+  }
+}
+
+export default UserConsolePanel
--- heroku/frontend/src/components/ConsoleTable/index.tsx
+++ heroku/frontend/src/components/ConsoleTable/index.tsx
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import LeftSideBar from './LeftSideBar'
+import UserConsolePanel from './UserConsolePanel'
 import '../../assets/third-party/admin-dashboard/styles/index.scss'
+import './ConsoleTable.scss'
 
 class ConsoleTable extends Component {
   render() {
     return (
       <div id="console-table">
         <Route path="/console" component={LeftSideBar} />
+        <div className="page-container">
+          <div className="console-content-container ">
+            <Switch>
+              <Route path="/console/user" component={UserConsolePanel} />
+            </Switch>
+          </div>
+        </div>
       </div>
     )
   }
